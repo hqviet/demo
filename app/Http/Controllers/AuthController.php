2b5de4ab314f5907da86871481443239255f8a6f
vlain121 <hqv1211@gmail.com>
2020-12-28 15:53:31 +0700

    init project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response()->json(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response()->json([ 'user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function detail(Request $req)
+    {
+        return response(['data' => Auth::user()]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(['message' => 'ok']);
+    }
+}
