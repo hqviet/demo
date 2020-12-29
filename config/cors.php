7f9854e3977344aba1d3676418bbdc0a6cc0f658
vlain121 <hqv1211@gmail.com>
2020-12-29 12:57:02 +0700

    updatte cors config

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*'],
+    'paths' => [],
 
     /*
     * Matches the request method. `['*']` allows all methods.
@@ -31,7 +31,7 @@ return [
     /*
      * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['*'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
